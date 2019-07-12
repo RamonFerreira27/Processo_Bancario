93bfcc7994c9c349fca8954983bb889b77eb18bf
Ramon Ferreira <ramon_ferreira27@hotmail.com>
2019-07-12 13:18:57 -0300

    Adicionar arquivos de projeto.

---
+++ Processo_Bancario/Conta_Bancaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processo_Bancario {
+    class Conta_Bancaria {
+        private int _conta;
+        private string _titular;
+        private double _saldo;
+
+        public int Conta {
+            get { return _conta; }
+            private set { _conta = value; }
+        }
+        public string Titular {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+        public double Saldo {
+            get { return _saldo; }
+            private set { _saldo = value; }
+        }
+
+        public Conta_Bancaria(int conta, string titular, double depositoInicial) {
+            Conta = conta;
+            Titular = titular;
+            Deposito(depositoInicial);
+        }
+        public Conta_Bancaria(int conta, string titular) {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public override string ToString() {
+            return "Conta: " + Conta
+                + ", Titular: " + Titular
+                + ", Saldo: $" + Saldo.ToString("F2");
+        }
+
+        public void Deposito(double valor) {
+            if (valor > 0) {
+                Saldo += valor;
+            }
+            else {
+                Environment.Exit(0);
+            }
+        }
+        public void Saque(double valor) {
+            Saldo -= valor + 5;
+        }
+    }
+}
---
+++ Processo_Bancario/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processo_Bancario {
+    class Program {
+        static void Main(string[] args) {
+            Conta_Bancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char escolha = char.Parse(Console.ReadLine());
+
+            if (escolha == 's') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Conta_Bancaria(numero, titular, depositoInicial);
+            }
+            else {
+                conta = new Conta_Bancaria(numero, titular);
+            }
+
+            Console.WriteLine("\nDados da conta:");
+            Console.WriteLine(conta);
+
+            Console.Write("\nEntre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            conta.Deposito(valor);
+            Console.WriteLine("\nDados de conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.Write("\nEntre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine());
+            conta.Saque(valor);
+            Console.WriteLine("\nDados de conta atualizados:");
+            Console.WriteLine(conta);
+            Console.ReadKey();
+        }
+    }
+}
