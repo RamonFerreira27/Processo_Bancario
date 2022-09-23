8ac15b88476a7027346092ef51e12cce79d96dcf
FERREIRA-PC\RamonFerreira <ramon_ferreira27@hotmail.com>
2022-09-23 02:51:18 -0300

    Refatorando código

---
+++ Processo_Bancario/Classes/Conta_Bancaria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processo_Bancario
+{
+    class Conta_Bancaria
+    {
+        public int      Conta   { get; private set; }
+        public string   Titular { get; set; }
+        public double   Saldo   { get; private set; }
+
+        public Conta_Bancaria(int conta, string titular, double depositoInicial)
+        {
+            Conta   = conta;
+            Titular = titular;
+            Deposito(depositoInicial);
+        }
+
+        public Conta_Bancaria(int conta, string titular)
+        {
+            Conta   = conta;
+            Titular = titular;
+        }
+
+        public override string ToString()
+        {
+            //// Primeira Forma que aprendi
+            //return "Conta: " + Conta
+            //    + ", Titular: " + Titular
+            //    + ", Saldo: $" + Saldo.ToString("F2");
+
+            // Segunda forma que aprendi
+            return String.Format("Conta: {0}, Titular: {1}, Saldo: ${2}", Conta, Titular, Saldo.ToString("F2"));
+
+            ////Terceira forma que aprendi
+            //return $"Conta: {Conta}, Titular: {Titular}, Saldo: ${Saldo.ToString("F2")}";
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor informado para depósito é inválido. Abortando...");
+                Environment.Exit(0);
+            }
+
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5;
+        }
+    }
+}
--- Processo_Bancario/Program.cs
+++ Processo_Bancario/Program.cs
@@ -4,42 +4,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Processo_Bancario {
-    class Program {
-        static void Main(string[] args) {
-            Conta_Bancaria conta;
-
-            Console.Write("Entre o número da conta: ");
-            int numero = int.Parse(Console.ReadLine());
-            Console.Write("Entre o titular da conta: ");
-            string titular = Console.ReadLine();
-            Console.Write("Haverá depósito inicial (s/n)? ");
-            char escolha = char.Parse(Console.ReadLine());
-
-            if (escolha == 's') {
-                Console.Write("Entre o valor de depósito inicial: ");
-                double depositoInicial = double.Parse(Console.ReadLine());
-                conta = new Conta_Bancaria(numero, titular, depositoInicial);
-            }
-            else {
-                conta = new Conta_Bancaria(numero, titular);
+namespace Processo_Bancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta_Bancaria conta = CriarConta();
+            if (conta == null)
+            {
+                Console.WriteLine("\nHouve algum erro na criação da conta. Abortando...");
+                Environment.Exit(0);
             }
 
             Console.WriteLine("\nDados da conta:");
             Console.WriteLine(conta);
 
-            Console.Write("\nEntre um valor para depósito: ");
-            double valor = double.Parse(Console.ReadLine());
-            conta.Deposito(valor);
-            Console.WriteLine("\nDados de conta atualizados:");
-            Console.WriteLine(conta);
+            RealizarOperacao("depósito", ref conta);
+            RealizarOperacao("saque", ref conta);
 
-            Console.Write("\nEntre um valor para saque: ");
-            valor = double.Parse(Console.ReadLine());
-            conta.Saque(valor);
+            Console.ReadKey();
+        }
+
+        private static void RealizarOperacao(string acao, ref Conta_Bancaria conta)
+        {
+            double valor = -1.0;
+
+            Console.Write(String.Format("\nEntre um valor para {0}: ", acao));
+            string entrada = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(entrada))
+            {
+                Console.WriteLine(String.Format("O valor informado para {0} é inválido. Abortando...", acao));
+                Environment.Exit(0);
+            }
+
+            if (!Double.TryParse(entrada, out valor))
+            {
+                Console.WriteLine(String.Format("Erro ao converter o valor de {0}. Abortando...", acao));
+                Environment.Exit(0);
+            }
+
+            if (acao.Equals("saque"))
+                conta.Saque(valor);
+            else
+                conta.Deposito(valor);
+
             Console.WriteLine("\nDados de conta atualizados:");
             Console.WriteLine(conta);
-            Console.ReadKey();
+        }
+
+        private static Conta_Bancaria CriarConta()
+        {
+            Conta_Bancaria conta = null;
+
+            Console.Write("Entre o número da conta: ");
+            string entrada = ResgatarValorDigitado("Número");
+            if (!Int32.TryParse(entrada, out int numero))
+            {
+                Console.WriteLine("\nO valor informado é inválido para este campo. Valor: {0} | Campo: Número", entrada);
+                Environment.Exit(0);
+            }
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = ResgatarValorDigitado("Titular");
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.S:
+                    {
+                        Console.Write("\nEntre o valor de depósito inicial: ");
+                        entrada = ResgatarValorDigitado("Número");
+                        if (!Double.TryParse(entrada, out double depositoInicial))
+                        {
+                            Console.WriteLine("\nO valor informado é inválido para este campo. Valor: {0} | Campo: Depósito Inicial", entrada);
+                            Environment.Exit(0);
+                        }
+
+                        conta = new Conta_Bancaria(numero, titular, depositoInicial);
+                        break;
+                    }
+                case ConsoleKey.N:
+                    conta = new Conta_Bancaria(numero, titular);
+                    break;
+                default:
+                    Console.WriteLine("\nOperação desconhecida. Abortando...");
+                    Environment.Exit(0);
+                    return null;
+            }
+
+            return conta;
+        }
+
+        private static string ResgatarValorDigitado(string campo)
+        {
+            string entrada = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(entrada))
+            {
+                Console.WriteLine("\nO valor informado é inválido para este campo. Valor: {0} | Campo: {1}", entrada, campo);
+                Environment.Exit(0);
+            }
+
+            return entrada;
         }
     }
 }
